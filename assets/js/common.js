026a69e34bfbcdfe3865a368e35c8ddbfa53541d
Lovell D'souza <lovelldies@gmail.com>
2017-05-31 00:16:55 +0530

    Some more cosmetic changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,13 @@
 jQuery( document ).ready(function() {
-  
   // Show/Hide the overlay menu.
   $('#logo').click(function () {
     $('#banner-nav-container').fadeToggle();
   });
   $('#close-nav').click(function () {
     $('#banner-nav-container').fadeOut();
+  });
+  $('#footermenulink').click(function () {
+    $('#banner-nav-container').fadeToggle();
   });
 
 });
