55fda0377b98f58f7f2d8f085ffc2b54dc696151
Lovell D'souza <lovelldies@gmail.com>
2017-05-31 12:50:07 +0530

    Final list of posts and some tweaking.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,12 @@
 jQuery( document ).ready(function() {
   // Show/Hide the overlay menu.
-  $('#logo').click(function () {
+  $('#logo, #footermenulink').click(function () {
     $('#banner-nav-container').fadeToggle();
   });
   $('#close-nav').click(function () {
     $('#banner-nav-container').fadeOut();
   });
-  $('#footermenulink').click(function () {
-    $('#banner-nav-container').fadeToggle();
+  $('img').bind('contextmenu', function(e) {
+    return false;
   });
-
 });
