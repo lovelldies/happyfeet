f82e4a66d5d3cf023bad4cfaa38da2016841914b
Lovell D'souza <lovelldies@gmail.com>
2017-07-03 22:42:25 +0530

    Layout, AMP and performance changes.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,6 @@
+---
+---
 jQuery( document ).ready(function() {
-  // Show/Hide the overlay menu.
   $('#logo, #footermenulink').click(function () {
     $('#banner-nav-container').fadeToggle();
   });
@@ -9,4 +10,37 @@ jQuery( document ).ready(function() {
   $('img').bind('contextmenu', function(e) {
     return false;
   });
+  generateRandomPosts();
 });
+
+function generateRandomPosts() {
+  $.getJSON("{{ site.baseurl }}/related-posts.json", function(data) {
+
+    var postsCount = data.length;
+    var posts = data;
+    var randomIndexUsed = [];
+    var counter = 0;
+    var numberOfPosts = 10;
+    var divRandomPosts = $("#random_posts .inner");
+
+    while (counter < numberOfPosts) {
+      var randomIndex = Math.floor(Math.random() * postsCount);
+
+      if (randomIndexUsed.indexOf(randomIndex) == "-1") {
+          var postHREF = posts[randomIndex].href;
+          var postTitle = posts[randomIndex].title;
+          var postImage = posts[randomIndex].image;
+          var postExcerpt = posts[randomIndex].excerpt;
+
+          if (counter == (numberOfPosts - 1)) {
+            divRandomPosts.append('<article><a href="' + postHREF + '" class="image"><img alt="'+ postTitle +'" src="' + postImage + '" alt="Alternate text" class="lazyload" /></a><div class="caption"> <h3>' + postTitle + '</h3><p>' + postExcerpt + '</p></div></article>');
+          } else {
+            divRandomPosts.append('<article><a href="' + postHREF + '" class="image"><img alt="'+ postTitle +'" src="' + postImage + '" alt="Alternate text" class="lazyload" /></a><div class="caption"> <h3>' + postTitle + '</h3><p>' + postExcerpt + '</p></div></article>');
+          }
+
+        randomIndexUsed.push(randomIndex);
+        counter++;
+      }
+    }
+  });
+}
